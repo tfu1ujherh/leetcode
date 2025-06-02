72e46d677fd31e99953ec220f0f7750f1bc93cfd
Ariga Bhavani Shankar <127045608+tfu1ujherh@users.noreply.github.com>
2025-06-02 07:45:11 +0530

    Time: 3 ms (84.79%) | Memory: 45.7 MB (70.42%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int candy(int[] ratings) {
+
+        int n=ratings.length;
+
+        int res[]=new int[n];
+
+        Arrays.fill(res,1);
+
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                res[i]=res[i-1]+1;
+            }
+        }
+
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                res[i]=Math.max(res[i],res[i+1]+1);
+            }
+        }
+
+        int sum=0;
+
+        for(int i=0;i<n;i++){
+            sum=sum+res[i];
+        }
+
+        return sum;
+        
+    }
+}
