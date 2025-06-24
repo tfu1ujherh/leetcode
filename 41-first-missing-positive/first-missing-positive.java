febb7a5248cda5393dae0eb6fcbdeb4ec1fa6fb3
Ariga Bhavani Shankar <127045608+tfu1ujherh@users.noreply.github.com>
2025-06-24 15:22:05 +0530

    Time: 17 ms (11.06%) | Memory: 66 MB (5.04%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        HashMap<Integer,Integer> hmap=new HashMap<>();
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            if(num>0){
+                hmap.put(num,i);
+            }
+        }
+        int i=1;
+        while(true){
+            if(!hmap.containsKey(i)){
+                return i;
+            }
+            i++;
+        }
+        
+    }
+}
