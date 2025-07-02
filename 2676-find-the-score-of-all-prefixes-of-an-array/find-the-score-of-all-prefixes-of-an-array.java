ecf5acc6bdfda2112be4a9cadb63fd8bee23d9cb
Ariga Bhavani Shankar <127045608+tfu1ujherh@users.noreply.github.com>
2025-07-02 13:54:16 +0530

    Time: 3 ms (82.81%) | Memory: 63.4 MB (42.19%) - LeetSync

--- 2676-find-the-score-of-all-prefixes-of-an-array/find-the-score-of-all-prefixes-of-an-array.java
+++ 2676-find-the-score-of-all-prefixes-of-an-array/find-the-score-of-all-prefixes-of-an-array.java
@@ -2,20 +2,32 @@ class Solution {
     public long[] findPrefixScore(int[] nums) {
         int n=nums.length;
         long res[]=new long[n];
-        res[0]=nums[0]*2;
-        int max=nums[0];
-        for(int i=1;i<n;i++){
-            // int num=nums[i];
-            max=Math.max(max,nums[i]);
-            if(nums[i]<max){
-                nums[i]=nums[i]+max;
-            }else{
-                nums[i]=nums[i]*2;
+        // res[0]=nums[0]*2;
+        // int max=nums[0];
+        // for(int i=1;i<n;i++){
+        //     max=Math.max(max,nums[i]);
+        //     if(nums[i]<max){
+        //         nums[i]=nums[i]+max;
+        //     }else{
+        //         nums[i]=nums[i]*2;
+        //     }
+        //     long cur=nums[i]+res[i-1];
+        //     res[i]=cur;
+        // }
+        // return res;
+        int max = 0;
+        for(int i=0;i<n;i++){
+            if(max<nums[i]){
+                max = nums[i];
             }
-            long cur=nums[i]+res[i-1];
-            res[i]=cur;
+            if(i==0){
+                res[i] = nums[i] + nums[i];
+            }
+            else{
+                int num = max + nums[i];
+                res[i] = num + res[i-1];
+            }
         }
         return res;
-        
     }
 }
