b24107caf11f110779c275224ff904dcacbc2624
Ariga Bhavani Shankar <127045608+tfu1ujherh@users.noreply.github.com>
2025-07-08 07:51:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (54.71%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+
+        int data=root.left.val+root.right.val;
+
+        if(root.val==data){
+            return true;
+        }
+        return false;
+        
+    }
+}
