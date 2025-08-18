51ee26c532e57957553af92e13e3573fe9e08075
Ariga Bhavani Shankar <127045608+tfu1ujherh@users.noreply.github.com>
2025-08-18 15:11:13 +0530

    Time: 1 ms (71.45%) | Memory: 42.2 MB (63.35%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+        
+
+        int len=solve(str1.length(),str2.length());
+        return str1.substring(0,len);
+    }
+
+
+    public static int solve(int len1,int len2){
+        while(len2!=0){
+            int temp=len1%len2;
+            len1=len2;
+            len2=temp;
+        }
+        return len1;
+    }
+}
