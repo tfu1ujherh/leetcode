e4f79e125a5e0438f2c4d55ce0578f1864e6f462
Ariga Bhavani Shankar <127045608+tfu1ujherh@users.noreply.github.com>
2025-08-26 11:38:59 +0530

    Time: 4 ms (21.67%) | Memory: 45.3 MB (97.00%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -2,11 +2,8 @@ class Solution {
     public int candy(int[] ratings) {
 
         int n=ratings.length;
-
         int res[]=new int[n];
-
         Arrays.fill(res,1);
-
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
                 res[i]=res[i-1]+1;
@@ -20,11 +17,9 @@ class Solution {
         }
 
         int sum=0;
-
         for(int i=0;i<n;i++){
-            sum=sum+res[i];
+            sum+=res[i];
         }
-
         return sum;
         
     }
