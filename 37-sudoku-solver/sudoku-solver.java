d13d6e1fb02c9cba37eba00a88f81d96a5f9dc6b
Ariga Bhavani Shankar <127045608+tfu1ujherh@users.noreply.github.com>
2025-08-31 11:30:50 +0530

    Time: 370 ms (35.62%) | Memory: 41.5 MB (25.13%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,12 +1,9 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-
         solve(board);
-        
     }
 
     public boolean solve(char board[][]){
-
         for(int row=0;row<9;row++){
             for(int col=0;col<9;col++){
                 if(board[row][col]=='.'){
@@ -30,13 +27,8 @@ class Solution {
     public boolean isValid(char board[][],int row,int col,char num){
 
         for (int i = 0; i < 9; i++) {
-            // Row
             if (board[row][i] == num) return false;
-
-            // Column
             if (board[i][col] == num) return false;
-
-            // 3x3 box
             int boxRow = 3 * (row / 3) + i / 3;
             int boxCol = 3 * (col / 3) + i % 3;
             if (board[boxRow][boxCol] == num) return false;
