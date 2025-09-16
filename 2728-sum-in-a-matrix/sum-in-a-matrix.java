46deda7313fda1c9623cf0fb697d9a7698c6c416
Ariga Bhavani Shankar <127045608+tfu1ujherh@users.noreply.github.com>
2025-09-16 22:19:08 +0530

    Time: 13 ms (99.21%) | Memory: 70.2 MB (59.65%) - LeetSync

---
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int matrixSum(int[][] nums) {
+        int m=nums.length;
+        int n=nums[0].length;
+        for(int i=0;i<m;i++){
+            Arrays.sort(nums[i]);
+        }
+
+        int res=0;
+        for(int i=n-1;i>=0;i--){
+            int max=0;
+            for(int j=0;j<m;j++){
+                max=Math.max(max,nums[j][i]);
+            }
+            res+=max;
+        }
+        return res;
+        
+    }
+}
